64ffcaf9a8105155e9be63fb96374a87677ffe8f
sung9920 <sungcheol9920@gmail.com>
2025-09-01 15:19:31 +0900

    spring #1

--- hellospring/src/main/java/com/bit2025/hellospring/controller/HelloController.java
+++ hellospring/src/main/java/com/bit2025/hellospring/controller/HelloController.java
@@ -2,12 +2,19 @@ package com.bit2025.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
 	@RequestMapping("/hello")
 	public String hello() {
+		return "/WEB-INF/views/hello.jsp";
+	}
+
+	@RequestMapping("/hello2")
+	public String hello2(@RequestParam("name") String name) {
+		System.out.println("name:" + name);
 		return "/WEB-INF/views/hello.jsp";
 	}
 }
