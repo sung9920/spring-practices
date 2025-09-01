30479c514cca0c388ff718b6af792e0ece53870f
sung9920 <sungcheol9920@gmail.com>
2025-09-01 15:53:53 +0900

    spring #2

--- hellospring/src/main/java/com/bit2025/hellospring/controller/HelloController.java
+++ hellospring/src/main/java/com/bit2025/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.bit2025.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
@@ -12,9 +14,18 @@ public class HelloController {
 		return "/WEB-INF/views/hello.jsp";
 	}
 
+	@ResponseBody
 	@RequestMapping("/hello2")
 	public String hello2(@RequestParam("name") String name) {
 		System.out.println("name:" + name);
 		return "/WEB-INF/views/hello.jsp";
+	}
+
+	@RequestMapping("/hello3")
+	public ModelAndView hello3(@RequestParam("n") String name) {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("/WEB-INF/views/hello3.jsp");
+		mav.addObject("name", name);
+		return mav;
 	}
 }
