238f6decca10b4a7259051a1cf799b8b1ef0d3e7
sung9920 <sungcheol9920@gmail.com>
2025-09-08 17:13:23 +0900

    emaillist03 transaction #3

--- emaillist03/src/main/java/com/bit2025/emaillist/service/EmaillistService.java
+++ emaillist03/src/main/java/com/bit2025/emaillist/service/EmaillistService.java
@@ -1,9 +1,16 @@
 package com.bit2025.emaillist.service;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.apache.taglibs.standard.tag.common.sql.DataSourceUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import com.bit2025.emaillist.repository.EmailRepository;
 import com.bit2025.emaillist.repository.EmaillogRepository;
@@ -11,6 +18,9 @@ import com.bit2025.emaillist.vo.EmailVo;
 
 @Service
 public class EmaillistService {
+	@Autowired
+	private DataSource dataSource;
+
 	@Autowired
 	private EmailRepository emailRepository;
 
@@ -26,13 +36,36 @@ public class EmaillistService {
 		if(count == 0) {
 			emaillogRepository.insert();
 		}
-		
+
 		emailRepository.insert(emailVo);
 	}
 
 	public void deleteEmail(Long id) {
-		EmailVo vo = emailRepository.findById(id);
-		emailRepository.deleteById(id);
-		emaillogRepository.update(vo.getRegDate());
+		// 트랜잭션 동기화(Connection) 작업: 초기화
+		TransactionSynchronizationManager.initSynchronization();
+		Connection conn = DataSourceUtils.getConnection(dataSource);
+
+		try {
+			// TX:begin ////////////////////////////////
+
+			conn.setAutoCommit(false);
+
+			EmailVo vo = emailRepository.findById(id);
+			emailRepository.deleteById(id);
+			emaillogRepository.update(vo.getRegDate());
+
+			// TX:end(success) ////////////////////////////////
+			conn.commit();
+		} catch(Throwable e) {
+			// TX:end(fail) ////////////////////////////////
+			try {
+				conn.rollback();
+			} catch (SQLException e1) {
+			}
+
+			throw new RuntimeException(e);
+		} finally {
+			DataSourceUtils.releaseConnection(conn, dataSource);
+		}
 	}
 }
